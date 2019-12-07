529ca0ddc46fa1d081aca768d335d59364f9de03
jdevjs <jesusalcalarojas@gmail.com>
2019-12-07 11:40:46 -0400

    add styles via css & color blue

--- app/routes/login/index.tsx
+++ app/routes/login/index.tsx
@@ -50,11 +50,14 @@ export default function LoginPage() {
     <Container style={{ height: '100vh' }}>
       <SectionContainer style={{ height: '100%' }}>
         <div className='form'>
-          <Typography style={{ marginBottom: '12px' }} color='dark'>
-            Iniciar Sesion
+          <svg viewBox='0 0 512 512'>
+            <path d='M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256  16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z' />
+          </svg>
+          <Typography style={{ marginBottom: '12px' }} as='h2' color='dark'>
+            Iniciar Sesión
           </Typography>
           <Input
-            text="Nombre de usuario"
+            text='Nombre de usuario'
             value={username}
             onChange={(e) => setUsername(e.currentTarget.value)}
             name='username'
@@ -62,7 +65,7 @@ export default function LoginPage() {
             placeholder='nombre de usuario'
           />
           <Input
-            text="Contraseña"
+            text='Contraseña'
             value={password}
             type='password'
             placeholder='contraseña de usuario'
