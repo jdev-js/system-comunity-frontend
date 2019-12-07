5ff5e77836cf36035d3d212a5f51b5d86033024f
jdevjs <jesusalcalarojas@gmail.com>
2019-12-07 08:19:16 -0400

    modificate point entry aplication

--- app/root.tsx
+++ app/root.tsx
@@ -4,14 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
+import './main.css'
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error)
+  return (
+    <Layout>
+      <div>
+        <h1>There was is error</h1>
+        <p>404</p>
+        <hr />
+      </div>
+    </Layout>
+  )
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -21,9 +35,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  )
 }
