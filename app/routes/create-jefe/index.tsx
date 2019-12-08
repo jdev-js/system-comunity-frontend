11338ca6728e2124982ffbff39d90405bedeac73
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:38:46 -0400

    add update handleSubmit and metaFunction & + field

--- app/routes/create-jefe/index.tsx
+++ app/routes/create-jefe/index.tsx
@@ -7,7 +7,20 @@ import Button from '~/components/button'
 import Form from '~/components/form'
 import React, { useState } from 'react'
 import { request } from '~/services/request'
-import { CREATE_JEFE } from '~/services/jefe'
+import { CREATE_JEFE, CreateJefe } from '~/services/jefe'
+import { useNavigate } from '@remix-run/react'
+import type { MetaFunction } from '@remix-run/node'
+import useAuth from '~/hooks/useAuth'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Crear un nuevo jefe de familia' },
+    {
+      name: 'description',
+      content: 'Registrar un nuevo jefe de familia',
+    },
+  ]
+}
 
 const initialvalues = {
   firstname: '',
@@ -15,11 +28,21 @@ const initialvalues = {
   phone: '',
   address: '',
   ci: '',
+  numberHome: '',
+  street: '',
+  typeDocument: '',
+  sexo: '',
+  numberBuy: '',
+  discapacity: '',
   dateNacimiento: '',
   age: '',
 }
-export default function CreateJefe() {
+export default function CreateJefePage() {
   const [values, setValues] = useState(initialvalues)
+  const [error, setError] = useState<null | string>(null)
+  const Auth = useAuth()
+
+  const navigate = useNavigate()
 
   const handleChange =
     (key: string) => (e: React.FormEvent<HTMLInputElement>) => {
@@ -30,11 +53,19 @@ export default function CreateJefe() {
     }
 
   const handleSubmit = async () => {
-    const [err, data] = await request(CREATE_JEFE, { input: { ...values } })
-    if (err) {
-      console.error(err)
+    const [err, data] = await request<CreateJefe>(CREATE_JEFE, {
+      idUser: Auth?.auth?.id ?? '',
+      input: { ...values },
+    })
+    if (data?.errors) {
+      setError(data?.errors[0].message)
     }
-    console.log(data)
+    if (err) {
+      setError(err.message)
+    }
+    if (data?.data.createJefe) {
+      navigate('/get-jefes')
+    }
   }
 
   return (
@@ -47,49 +78,122 @@ export default function CreateJefe() {
           </Typography>
           <Form>
             <Input
+              text='Nombre completo'
+              value={values.firstname}
               onChange={handleChange('firstname')}
               size='md'
               placeholder='nombre completo'
             />
             <Input
+              text='Apellido completo'
+              value={values.lastname}
               onChange={handleChange('lastname')}
               size='md'
               placeholder='apellido completo'
             />
             <Input
+              text='Numero telefeno'
+              value={values.phone}
               onChange={handleChange('phone')}
               size='md'
               placeholder='numero de telefeno'
             />
             <Input
+              text='Dirección'
+              value={values.address}
               onChange={handleChange('address')}
               size='md'
               placeholder='direccion'
             />
             <Input
+              text='Cedula de idéntidad'
+              value={values.ci}
               onChange={handleChange('ci')}
               size='md'
-              placeholder='cedula de identidad'
+              placeholder='cedula de idéntidad'
             />
             <Input
+              text='Numero de Calle'
+              value={values.street}
+              onChange={handleChange('street')}
+              size='md'
+              placeholder='numero de calle'
+            />
+            <Input
+              text='Tipo de documento'
+              value={values.typeDocument}
+              onChange={handleChange('typeDocument')}
+              size='md'
+              placeholder='tipo de documento'
+            />
+            <Input
+              text='Numero de casa'
+              value={values.numberHome}
+              onChange={handleChange('numberHome')}
+              size='md'
+              placeholder='tipo de documento'
+            />
+            <Input
+              text='Numero de compras'
+              value={values.numberBuy}
+              onChange={handleChange('numberBuy')}
+              size='md'
+              placeholder='numero de compras'
+            />
+            <Input
+              text='Sexo'
+              value={values.sexo}
+              onChange={handleChange('sexo')}
+              size='md'
+              placeholder='sexo'
+            />
+            <Input
+              text='Discapicidad o Enfermedad'
+              value={values.discapacity}
+              onChange={handleChange('discapacity')}
+              size='md'
+              placeholder='discacidad o enfermedad'
+            />
+            <Input
+              text='Fecha de nacimiénto'
+              value={values.dateNacimiento}
               onChange={handleChange('dateNacimiento')}
               size='md'
-              placeholder='fecha de nacimiento'
+              placeholder='fecha de nacimiénto'
             />
             <Input
+              text='Edad'
+              value={values.age}
               onChange={handleChange('age')}
               size='md'
               placeholder='edad'
             />
+            <Button
+              onClick={handleSubmit}
+              style={{ marginTop: '20px' }}
+              color='primary'
+              size='lg'
+            >
+              Crear jefe
+            </Button>
           </Form>
-          <Button
-            onClick={handleSubmit}
-            style={{ marginTop: '20px' }}
-            color='primary'
-            size='lg'
-          >
-            Crear jefe
-          </Button>
+          {error && (
+            <Typography
+              className=''
+              style={{
+                margin: '10px auto',
+                padding: '2px 8px',
+                borderRadius: '5px',
+                backgroundColor: 'var(--bg-error)',
+                color: 'var(--text-error)',
+              }}
+              color='dark'
+              as='p'
+              variant='md'
+            >
+              {error}
+            </Typography>
+          )}
         </SectionContainer>
       </Container>
     </Container>
