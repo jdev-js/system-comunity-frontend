cf7e6311f5567c1c11836d9f6b1fc54ae92bb6af
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:24:21 -0400

    add logic and props onUpdate and validation

--- app/components/list-item-jefe.tsx
+++ app/components/list-item-jefe.tsx
@@ -1,8 +1,15 @@
+import { Jefe } from '~/services/jefe'
 import CardJefe from './card-jefe'
 import ContainerList from './container-list'
 import Typography from './typography'
 
-export default function ListItemJefe({ data }) {
+export default function ListItemJefe({
+  data,
+  onUpdate,
+}: {
+  data: Jefe[] | null
+  onUpdate: () => void
+}) {
   return (
     <ContainerList>
       <Typography
@@ -12,9 +19,27 @@ export default function ListItemJefe({ data }) {
       >
         Jefes de familia
       </Typography>
-      {data.map((jefe, index) => (
-        <CardJefe key={index} data={jefe} />
-      ))}
+      {data === null ? (
+        <Typography
+          color='dark'
+          variant='3xl'
+          style={{ marginTop: '20px', marginBottom: '30px' }}
+        >
+          No se encuentran jefes de familia
+        </Typography>
+      ) : data.length > 0 ? (
+        data.map((jefe, index) => (
+          <CardJefe onUpdate={onUpdate} key={index} data={jefe} />
+        ))
+      ) : (
+        <Typography
+          color='dark'
+          variant='3xl'
+          style={{ marginTop: '20px', marginBottom: '30px' }}
+        >
+          No hay jefes de familia registrados
+        </Typography>
+      )}
     </ContainerList>
   )
 }
