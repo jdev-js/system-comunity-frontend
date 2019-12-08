a79055637be581787876cb3e304e152fd4df2f0c
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:49:55 -0400

    add SEO

--- app/routes/create-miembro/index.tsx
+++ app/routes/create-miembro/index.tsx
@@ -7,19 +7,38 @@ import Button from '~/components/button'
 import Form from '~/components/form'
 import { useState } from 'react'
 import { request } from '~/services/request'
-import { CREATE_MIEMBRO } from '~/services/miembro'
+import { CREATE_MIEMBRO, CreateMiembro } from '~/services/miembro'
+import { useNavigate } from '@remix-run/react'
+import type { MetaFunction } from '@remix-run/node'
+import useAuth from '~/hooks/useAuth'
 
-const initialvalues = {
-  ciJefe: '',
-  firstname: '',
-  lastname: '',
-  ci: '',
-  dateNacimiento: '',
-  age: '',
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Crear un nuevo miembro de familia' },
+    {
+      name: 'description',
+      content: 'Registrar un nuevo miembro de familia',
+    },
+  ]
 }
 
-export default function CreateMiembro() {
+const initialvalues = {
+  firstname: '',
+  lastname: '',
+  phone: '',
+  age: '',
+  dateNacimiento: '',
+  ciJefeFamily: '',
+  discapacity: '',
+  ci: '',
+  sexo: '',
+}
+
+export default function CreateMiembroPage() {
   const [values, setValues] = useState(initialvalues)
+  const [error, setError] = useState<null | string>(null)
+  const Auth = useAuth()
+  const navigate = useNavigate()
 
   const handleChange =
     (key: string) => (e: React.FormEvent<HTMLInputElement>) => {
@@ -30,11 +49,19 @@ export default function CreateMiembro() {
     }
 
   const handleSubmit = async () => {
-    const [err, data] = await request(CREATE_MIEMBRO, { input: { ...values } })
-    if (err) {
-      console.error(err)
+    const [err, data] = await request<CreateMiembro>(CREATE_MIEMBRO, {
+      idUser: Auth?.auth?.id,
+      input: { ...values },
+    })
+    if (data?.errors) {
+      setError(data?.errors[0].message)
     }
-    console.log(data)
+    if (err) {
+      setError(err.message)
+    }
+    if (data?.data.createMiembro) {
+      navigate('/get-miembros')
+    }
   }
 
   return (
@@ -47,44 +74,93 @@ export default function CreateMiembro() {
           </Typography>
           <Form>
             <Input
-              onChange={handleChange('ciJeje')}
+              text='Cedula del jefe de familia'
+              value={values.ciJefeFamily}
+              onChange={handleChange('ciJefeFamily')}
               size='md'
               placeholder='cedula del jefe de familia'
             />
             <Input
+              text='Nombre completo'
+              value={values.firstname}
               onChange={handleChange('firstname')}
               size='md'
               placeholder='nombre completo'
             />
             <Input
+              text='Apellido completo'
+              value={values.lastname}
               onChange={handleChange('lastname')}
               size='md'
               placeholder='apellido completo'
             />
             <Input
+              text='Cedula de idéntidad'
+              value={values.ci}
               onChange={handleChange('ci')}
               size='md'
-              placeholder='cedula de identidad'
+              placeholder='cedula de idéntidad'
             />
             <Input
+              text='Fecha de nacimiénto'
+              value={values.dateNacimiento}
               onChange={handleChange('dateNacimiento')}
               size='md'
-              placeholder='fecha de nacimiento'
+              placeholder='fecha de nacimiénto'
             />
             <Input
+              text='Edad'
+              value={values.age}
               onChange={handleChange('age')}
               size='md'
               placeholder='edad'
             />
+            <Input
+              text='Telefono o Email'
+              value={values.phone}
+              onChange={handleChange('phone')}
+              size='md'
+              placeholder='telefono o email'
+            />
+            <Input
+              text='Discapacidad o Enfermedad'
+              value={values.discapacity}
+              onChange={handleChange('discapacity')}
+              size='md'
+              placeholder='discapacidad o enfermedad'
+            />
+            <Input
+              text='Sexo'
+              value={values.sexo}
+              onChange={handleChange('sexo')}
+              size='md'
+              placeholder='sexo'
+            />
+            <Button
+              onClick={handleSubmit}
+              color='primary'
+              style={{ marginTop: '20px' }}
+              size='lg'
+            >
+              Crear miembro
+            </Button>
           </Form>
-          <Button
-            onClick={handleSubmit}
-            style={{ marginTop: '20px' }}
-            color='primary'
-            size='lg'
-          >
-            Crear miembro
-          </Button>
+          {error && (
+            <Typography
+              style={{
+                margin: '10px auto',
+                padding: '2px 8px',
+                borderRadius: '5px',
+                backgroundColor: 'var(--bg-error)',
+                color: 'var(--text-error)',
+              }}
+              color='dark'
+              as='p'
+              variant='md'
+            >
+              {error}
+            </Typography>
+          )}
         </SectionContainer>
       </Container>
     </Container>
