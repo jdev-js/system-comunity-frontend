efef911f11be0acb3ddacafa1a5d3f594871da4f
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 06:11:55 -0400

    add SEO & MetaFunction & logic

--- app/routes/login/index.tsx
+++ app/routes/login/index.tsx
@@ -7,16 +7,43 @@ import Container from '~/components/container'
 import SectionContainer from '~/components/section-container'
 import { LOGIN, DataLogin } from '~/services/user'
 import { request } from '~/services/request'
+import { useNavigate } from '@remix-run/react'
+import useAuth from '~/hooks/useAuth'
+
+import type { MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Iniciar Sesión' },
+    {
+      name: 'description',
+      content:
+        'Iniciar sesion, para poder administrar los datos de las personas de la comunidad',
+    },
+  ]
+}
 
 export default function LoginPage() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const Auth = useAuth()
+  const [error, setError] = useState<null | string>(null)
   const handleSubmit = async () => {
-    const [err, data] = await request<DataLogin>(LOGIN, { username, password })
-    if (err) {
-      console.error(err)
+    const [err, data] = await request<DataLogin>(LOGIN, {
+      username,
+      password,
+    })
+    if (data) {
+      if (data?.errors) {
+        setError(data?.errors[0]?.message)
+      } else if (err) {
+        setError(err.message)
+      } else {
+        Auth?.changeAuth(data.data.login)
+        navigate('/admin')
+      }
     }
-    console.log(data)
   }
 
   return (
@@ -39,6 +66,19 @@ export default function LoginPage() {
             placeholder='contraseña de usuario'
             onChange={(e) => setPassword(e.currentTarget.value)}
           />
+          {error && (
+            <Typography
+              as='p'
+              variant='sm'
+              style={{
+                color: 'var(--text-error)',
+                textAlign: 'center',
+                margin: '5px auto',
+              }}
+            >
+              {error}
+            </Typography>
+          )}
           <Button
             onClick={handleSubmit}
             style={{ marginTop: '10px' }}
