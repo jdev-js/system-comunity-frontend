e10109df66aec9844c8b3768e1c3b0d5a8a41101
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 06:09:11 -0400

    add modifcate types request and logic request

--- app/services/request.ts
+++ app/services/request.ts
@@ -2,15 +2,25 @@ import { BASE_URL } from './base-url'
 
 export const request = async <T>(
   body: string,
-  variables: { input: Record<string, string> } | Record<string, string>
+  variables?: { input: Record<string, string> } | Record<string, string>
 ): Promise<[Error?, T?]> => {
+  let bodyRequest
+
+  if (variables) {
+    bodyRequest = {
+      query: body,
+      variables,
+    }
+  } else {
+    bodyRequest = {
+      query: body,
+    }
+  }
+
   try {
     const res = await fetch(`${BASE_URL}`, {
       method: 'POST',
-      body: JSON.stringify({
-        query: body,
-        variables,
-      }),
+      body: JSON.stringify(bodyRequest),
       headers: {
         'Content-Type': 'application/json',
       },
