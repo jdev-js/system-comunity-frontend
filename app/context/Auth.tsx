faf8bf0a70ebbf919c50ac0231e5a4091535b060
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:30:07 -0400

    add authentication plus method logout + values

--- app/context/Auth.tsx
+++ app/context/Auth.tsx
@@ -1,37 +1,46 @@
 import { createContext, useEffect, useState } from 'react'
 
 type User = {
-    name: string
-    id: string
+  firstname: string
+  lastname: string
+  role: string
+  id: string
 }
 
 type AuthValue = {
-    auth: null | User
-    changeAuth: (newValue: User) => void
+  auth: null | User
+  changeAuth: (newValue: User) => void
+  logout: () => void
 }
 
 export const authContex = createContext<AuthValue | null>(null)
 
-export function AuthProvider({children}: { children: React.ReactNode }){
-    const [auth,setAuth] = useState<User | null>(null)
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [auth, setAuth] = useState<User | null>(null)
 
-    const changeAuth = (newValue: User) => {
-        setAuth(newValue)
+  const changeAuth = (newValue: User) => {
+    setAuth(newValue)
+    window.localStorage.setItem('auth', JSON.stringify(newValue))
+  }
+
+  const logout = () => {
+    setAuth(null)
+    window.localStorage.clear()
+  }
+
+  useEffect(() => {
+    const userString = window.localStorage.getItem('auth')
+    if (userString !== null) {
+      const userParser = JSON.parse(userString)
+      if (userParser !== null) {
+        setAuth(userParser)
+      }
     }
+  }, [])
 
-    useEffect(() => {
-        const userString = window.localStorage.getItem('auth')
-        if(userString !== null){
-            const userParser = JSON.parse(userString ?? '')
-            if(userParser !== null){
-                setAuth(userParser)
-            }
-        }
-    },[])
-
-    return(
-        <authContex.Provider value={{auth,changeAuth}}>
-            {children}
-        </authContex.Provider>
-    )
+  return (
+    <authContex.Provider value={{ auth, changeAuth, logout }}>
+      {children}
+    </authContex.Provider>
+  )
 }
