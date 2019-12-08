743d6d711753af0461bdc9aa91aaeb4837d1072f
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:18:06 -0400

    add component Typography and props text

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,4 +1,5 @@
 import './component.css'
+import Typography from './typography'
 
 interface InputProps extends React.HtmlHTMLAttributes<HTMLInputElement> {
   type?: string
@@ -6,19 +7,27 @@ interface InputProps extends React.HtmlHTMLAttributes<HTMLInputElement> {
   name?: string
   size?: 'sm' | 'lg' | 'md' | 'full'
   value?: string
+  text?: string
 }
 
 export default function Input({
   className,
   size = 'lg',
   value = '',
+  text = '',
   ...props
 }: InputProps) {
   return (
-    <input
-      value={value}
-      className={`input input-size-${size} ${className}`}
-      {...props}
-    />
+    <div className={`container-input container-input-size-${size}`}>
+      <Typography style={{ marginLeft: '5px', fontSize: '14px' }} color='dark'>
+        {text}
+      </Typography>
+      <input
+        value={value}
+        style={{ width: '90%' }}
+        className={`input ${className}`}
+        {...props}
+      />
+    </div>
   )
 }
