9401b528844ac963e00358b0b2a4aafd530dd8e7
jdevjs <jesusalcalarojas@gmail.com>
2019-12-08 05:07:43 -0400

    remove numberPerson the props

--- app/components/card-info.tsx
+++ app/components/card-info.tsx
@@ -2,20 +2,14 @@ import UserIcon from './icons/user'
 import './component.css'
 
 interface CardInfoProps {
-  numberPerson?: string | number
   title?: string
   description?: string
 }
 
-export default function CardInfo({
-  numberPerson,
-  title,
-  description,
-}: CardInfoProps) {
+export default function CardInfo({ title, description }: CardInfoProps) {
   return (
     <article className='card'>
       <div className='card-info'>
-        <strong>+{numberPerson?.toString()}</strong>
         <UserIcon width='35' height='35' />
       </div>
       <h4>{title}</h4>
